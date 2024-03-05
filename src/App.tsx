2adbdfbefe90d3fd8102bbb9027e7ce0996691a9
jd1010756 <106291794+j1010756@users.noreply.github.com>
2024-03-04 22:15:11 -0700

    finishassignment

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+// import info from file
+import teamInfo from './CollegeBasketballTeams.json';
+
+// define the type for each team in the JSON file
+interface Team {
+  school: string;
+  name: string;
+  city: string;
+  state: string;
+}
+
+// function to introduce user to the site
+function Intro() {
+  return <h1>March Madness Special: NCAA Basketball Teams</h1>;
+}
+
+// component for a single team
+class TeamCard extends React.Component<Team> {
+  render() {
+    // select attributes to display
+    const { school, name, city, state } = this.props;
+
+    // return info for single team
+    return (
+      <div>
+        <h2>{school}</h2>
+        <h3>Mascot Name: {name}</h3>
+        <h3>
+          Location: {city}, {state}
+        </h3>
+        <br />
+      </div>
+    );
+  }
+}
+
+// function to render a list of teams
+function TeamList() {
+  // access the 'teams' array from the imported JSON data
+  const teams = teamInfo.teams;
+
+  // return list of all teams
+  return (
+    <div>
+      {teams.map((oneTeam: Team, index: number) => (
+        <TeamCard key={index} {...oneTeam} />
+      ))}
+    </div>
+  );
+}
+
+// app component
 function App() {
+  // return two visual functions (intro, teamlist)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Intro />
+      <TeamList />
     </div>
   );
 }
